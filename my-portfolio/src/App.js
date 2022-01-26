63f0e486b4028e79f8ce3d23a3b4d683b5de959a
Manoj Chowdary <manojchow72@gmail.com>
2022-01-26 20:35:36 +0530

    First Commit to my portfolio

---
+++ my-portfolio/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Click on the link to view my Github Profile.
+        </p>
+        <a
+          className="App-link"
+          href="https://github.com/Manojkumarchow"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Manoj's Portfolio
+        </a>
+        {/* <img src="name.jpg" /> */}
+      </header>
+    </div>
+  );
+}
+
+export default App;
